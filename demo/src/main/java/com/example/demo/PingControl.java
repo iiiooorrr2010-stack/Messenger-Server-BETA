ffb782783b6b2812c87e3b459eeb3674ad4f1c61
Kezess <iiiooorrr2010@gmail.com>
2026-04-14 01:11:55 +0700

    Add files via upload
    final 2.2 commit

---
+++ demo/src/main/java/com/example/demo/PingControl.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingControl {
+@GetMapping("/ping")
+    public String ping() {
+    return "ЛЕВ ХУЕСОС ЕБУЧИЙ БЛЯТЬ УМРИ НАХУЙ";
+}
+
+}
